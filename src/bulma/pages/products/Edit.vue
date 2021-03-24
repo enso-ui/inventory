3516982a1d7125b99ebf9996107c0d51ddd4a569
mishu <mishu@xtelecom.ro>
2021-03-24 11:02:40 +0200

    updates product components; moves icons to src

--- src/bulma/pages/products/Edit.vue
+++ src/bulma/pages/products/Edit.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="columns is-centered">
         <div class="column is-three-quarters-desktop is-full-touch">
-            <enso-form class="box has-background-light raises-on-hover"/>
+            <base-form/>
             <accessories>
                 <template slot-scope="{ count }">
                     <tab keep-alive
@@ -25,7 +25,6 @@
                         <div class="columns is-centered">
                             <div class="column is-two-thirds">
                                 <documents :id="id"
-                                    compact
                                     type="product"
                                     @update="$set(count, 'Documents', $refs.documents.count)"
                                     ref="documents"/>
@@ -37,23 +36,20 @@
         </div>
     </div>
 </template>
-
 <script>
-import { Accessories, Documents, Comments } from '@enso-ui/bulma';
-import { Tab } from '@enso-ui/tabs/bulma';
-import { EnsoForm } from '@enso-ui/forms/bulma';
-import PositionsManager from '../../components/PositionsManager.vue';
-
+import {
+    Accessories, Documents, Comments, Tab,
+} from '@enso-ui/bulma';
+import PositionsManager from '@enso-ui/inventory/src/bulma/components/PositionsManager.vue';
+import BaseForm from './components/BaseForm.vue';
 export default {
     name: 'Edit',
-
     components: {
-        EnsoForm, PositionsManager, Accessories, Documents, Comments, Tab,
+        BaseForm, Accessories, Tab, Documents, Comments, PositionsManager,
     },
-
     computed: {
         id() {
-            return Number.parseInt(this.$route.params.product, 10);
+            return this.$route.params.product;
         },
     },
 };
--- src/bulma/pages/products/Index.vue
+++ src/bulma/pages/products/Index.vue
@@ -1,22 +1,32 @@
 <template>
     <div>
-        <div class="columns is-centered"
-            v-if="ready">
+        <div class="columns is-centered is-multiline"
+             v-if="ready">
             <div class="column is-narrow">
                 <enso-date-filter class="box raises-on-hover"
                     v-model="params.dateInterval"
-                    default="thirtyDays"
+                    default="thisMonth"
                     :name="i18n('Updated')"
                     :interval="intervals.products.updated_at"/>
             </div>
+            <div class="column is-narrow">
+                <enso-interval-filter class="box raises-on-hover narrow-filter"
+                    :name="i18n('Quantity')"
+                    type="number"
+                    v-model="intervals.inventory_stocks.quantity"/>
+            </div>
         </div>
         <enso-table class="box is-paddingless raises-on-hover"
-            ref="stocks"
-            id="products"
+            ref="products"
             :intervals="intervals"
             :params="params"
-            @manage-positions="productId=$event.dtRowId"
+            id="products"
+            @manage-positions="productId=$event.id"
             @reset="$refs.filterState.reset()">
+            <template v-slot:stock="{ row }">
+                <product-locations :product="row"
+                    :classes="'is-info is-raised is-bold is-clickable'"/>
+            </template>
             <template v-slot:pictureUrl="{ row }">
                 <figure class="image product-image is-48x48 has-vertically-centered-content">
                     <a :href="row.pictureUrl" target="_blank">
@@ -25,93 +35,92 @@
                 </figure>
             </template>
         </enso-table>
-        <modal v-if="productId"
-            @keyup.esc="close"
-            @close="close">
-            <positions-manager :product-id="productId"/>
-        </modal>
         <filter-state :api-version="apiVersion"
             name="product_filters"
-            :filters="filters"
             :intervals="intervals"
             :params="params"
             @ready="ready = true"
             ref="filterState"/>
+        <modal show
+            v-if="productId"
+            @keyup.esc="close"
+            @close="close">
+            <positions-manager :product-id="productId"/>
+        </modal>
     </div>
 </template>
-
 <script>
+import { mapState } from 'vuex';
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { faBoxOpen } from '@fortawesome/free-solid-svg-icons';
-import { Modal } from '@enso-ui/modal/bulma';
-import { EnsoTable, EnsoDateFilter } from '@enso-ui/tables/bulma';
-import { FilterState } from '@enso-ui/filters/renderless';
-import PositionsManager from '../../components/PositionsManager.vue';
-
+import {
+    Modal, EnsoDateFilter, EnsoFilter,
+    EnsoSelectFilter, EnsoIntervalFilter, FilterState,
+} from '@enso-ui/bulma';
+import { EnsoTable } from '@enso-ui/tables/bulma';
+import PositionsManager from '@enso-ui/inventory/src/bulma/components/PositionsManager.vue';
+import ProductLocations from '@enso-ui/inventory/src/bulma/components/ProductLocations.vue';
 library.add(faBoxOpen);
-
 export default {
-
+    name: 'Index',
     components: {
+        ProductLocations,
         Modal,
         PositionsManager,
         EnsoTable,
         EnsoDateFilter,
+        EnsoFilter,
+        EnsoSelectFilter,
+        EnsoIntervalFilter,
         FilterState,
     },
-
-    inject: ['i18n'],
-
+    inject: ['i18n', 'route'],
     data() {
         return {
-            apiVersion: 1.2,
+            apiVersion: 1.0,
             ready: false,
             productId: null,
-            filters: {
-                products: {
-                    manufacturer_id: null,
-                },
+            params: {
+                dateInterval: 'thisMonth',
             },
             intervals: {
-                products: {
-                    updated_at: {
+                inventory_stocks: {
+                    quantity: {
                         min: null,
                         max: null,
                     },
                 },
-            },
-            params: {
-                mapping: false,
-                supplierId: null,
-                authorId: null,
-                dateInterval: 'all',
-            },
-            authorParams: {
-                is_author: true,
+                products: {
+                    updated_at: {
+                        min: null,
+                        max: null,
+                        dateFormat: null,
+                    },
+                },
             },
         };
     },
-
+    computed: {
+        ...mapState(['meta', 'enums']),
+    },
     methods: {
         close() {
             this.productId = null;
-            this.$refs.stocks.fetch();
+            this.$refs.products.fetch();
         },
     },
 };
 </script>
-
 <style lang="scss">
-    .image.product-image.is-48x48 > a {
-        width: 48px;
-        height: 48px;
-
-        img {
-            margin: auto;
-            width: auto;
-            height: auto;
-            max-width: 48px;
-            max-height: 48px;
-        }
+.image.product-image.is-48x48 > a {
+    width: 48px;
+    height: 48px;
+    img {
+        margin: auto;
+        width: auto;
+        height: auto;
+        max-width: 48px;
+        max-height: 48px;
     }
+}
 </style>
